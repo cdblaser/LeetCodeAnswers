5a5f1239f5d1f670ee5ab82a667d6332f4e5c152
Curtis Blaser <cd13laser@gmail.com>
2022-10-28 23:03:51 -0400

    first commit

---
+++ 112_Path-sum.js
@@ -0,0 +1,16 @@
+var hasPathSum = function (root, targetSum) {
+  const recursiveFxn = (root, sum) => {
+    if (!root) return false;
+
+    if (!root.left && !root.right) {
+      let test = sum + root.val;
+      if (test === targetSum) return true;
+    }
+
+    return (
+      recursiveFxn(root.right, sum + root.val) ||
+      recursiveFxn(root.left, sum + root.val)
+    );
+  };
+  return recursiveFxn(root, 0);
+};
